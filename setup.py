527538f686fd92d9d02e93102dbba3bbbe552109
Ramanand599 <ramanandchauhan599@gmail.com>
2024-01-27 21:45:41 +0530

    updated the exception and logger python file

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ HYPEN_E_DOT="-e ."
 def get_requirements(file_path:str)->List[str]:
     """
     This function will return the list of requirements.
-    
     """
     requirements = []
     with open(file_path) as file_obj:
---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Generate a detailed error message with information from the exception.
+
+    Args:
+        error (Exception): The exception instance.
+        error_detail (sys): The sys module instance.
+
+    Returns:
+        str: Formatted error message.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in Python script {file_name}, line {exc_tb.tb_lineno}: {str(error)}"
+    return error_message
+
+class CustomException(Exception):
+    """
+    Custom exception class with additional detailed error message.
+
+    Args:
+        error_message (str): The original error message.
+        error_detail (sys): The sys module instance.
+    """
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+    
