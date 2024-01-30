4563744d68dbb851f5baddcea83cbae5f4ae66c8
Ramanand599 <ramanandchauhan599@gmail.com>
2024-01-30 19:20:22 +0530

    updating data_transformation.py file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -5,3 +5,48 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer  # used to create a pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+
+from src.exception import CustomException
+from src.logger import logging
+import os 
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts","preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            numerical_columns = ["writing_score","reading_score"]
+            categorical_columns = [
+                "gender"
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course"
+            ]
+            num_pipeline = Pipeline(
+                steps = [
+                    ("imputer",SimpleImputer(strategy="meadian")),
+                    ("scaler",StandardScaler())
+
+                ]
+            )
+            cat_pipeline = Pipeline(
+                steps = [
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler())
+                ]
+            )
+            logging.info("Numerical columns standerd scaling completed.")
+            logging.info("Categorical columns encoding completed.")
+
+
+            
+        except:
+            pass
