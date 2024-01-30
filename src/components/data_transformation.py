ea9c176d9adbe151d38e7e8fe0fce1c600035816
Ramanand599 <ramanandchauhan599@gmail.com>
2024-01-30 19:50:30 +0530

    updating data_transformation.py file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -87,8 +87,10 @@ class DataTransformation:
             train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
             logging.info(f"Saved preprocessing object.")
+            
             save_object(
-                
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
             )
 
             return (
