c0cbe805a35e3777b39bd063561903393ec2c311
Ramanand599 <ramanandchauhan599@gmail.com>
2024-01-31 20:04:47 +0530

    model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -6,7 +6,7 @@ from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
-    RandomForestRegressor
+    RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -21,16 +21,17 @@ from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
+
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Split training and test input data.")
-            X_train,y_train,X_test,y_test = (
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
@@ -44,7 +45,7 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-            }  
+            }
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -81,8 +82,11 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 }
                 
-            } 
-            model_report:dict = evaluate_models(X_train=X_train,y_test=y_test,X_test=X_test,y_test=y_test,models=models,param=params)
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
@@ -106,5 +110,10 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+            
+
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
