99cccee54a7585b121081596dd0e8494f2f656a6
Ramanand599 <ramanandchauhan599@gmail.com>
2024-01-31 19:38:51 +0530

    model_trainer.py

--- src/exception.py
+++ src/exception.py
@@ -12,9 +12,10 @@ def error_message_detail(error, error_detail: sys):
     Returns:
         str: Formatted error message.
     """
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = f"Error occurred in Python script {file_name}, line {exc_tb.tb_lineno}: {str(error)}"
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+
     return error_message
 
 class CustomException(Exception):
@@ -25,10 +26,10 @@ class CustomException(Exception):
         error_message (str): The original error message.
         error_detail (sys): The sys module instance.
     """
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
     
